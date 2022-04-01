193b4692fb63e77d9e851282995a79cc1c30319d
Christian Carnevale <cncarnevale@gmail.com>
2022-03-31 23:30:29 -0400

    added timer, variables, 5 functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 var startcontainer = document.querySelector(".startContainer");
 var startbutton = document.querySelector(".startbtn");
 var questionContainer = document.querySelector(".questionContainer");
-var timer = document.querySelector(".timer");
+var secondsLeft = document.querySelector(".seconds");
 var score = document.querySelector(".score");
 var question = document.querySelector(".question");
 var answerList = document.querySelector(".answerList");
@@ -10,7 +10,10 @@ var answer1 = document.querySelector(".answer1");
 var answer2 = document.querySelector(".answer2");
 var answer3 = document.querySelector(".answer3");
 var answer4 = document.querySelector(".answer4");
+var answerButton = document.querySelector(".answerButton");
+var timer = document.querySelector(".timer");
 var questionIndex = 0;
+
 
 
 
@@ -48,6 +51,12 @@ timer.style.display = 'none';
 score.style.display = 'none';
 question.style.display = 'none';
 highScores.style.display = 'none';
+answer1.style.display = 'none';
+answer2.style.display = 'none';
+answer3.style.display = 'none';
+answer4.style.display = 'none';
+
+
 
 function startQuiz (){
   console.log("startQuiz");
@@ -57,6 +66,7 @@ function startQuiz (){
   highScores.style.display = 'block';
   displayQuestion(0);
   //start timer
+  setTime();
 
 }
 
@@ -65,6 +75,10 @@ function displayQuestion (i){
     console.log(questions[i].question);
     console.log(questions[i].choices);
     question.textContent=questions[i].question;
+    answer1.style.display = 'block';
+    answer2.style.display = 'block';
+    answer3.style.display = 'block';
+    answer4.style.display = 'block';
     answer1.textContent=questions[i].choices[0];
     answer2.textContent=questions[i].choices[1];
     answer3.textContent=questions[i].choices[2];
@@ -73,52 +87,53 @@ function displayQuestion (i){
 }
 //add event listenerfor answerButton to call functio
 // 
+answerButton.addEventListener("click",nextQuestion);
+answer1.addEventListener("click",checkAnswer);
+answer2.addEventListener("click",checkAnswer);
+answer3.addEventListener("click",checkAnswer);
+answer4.addEventListener("click",checkAnswer);
 
-/*function checkAnswer(){
-  if true {}
-}
+var key = [questions.answers[0],questions.answers[1]];
+console.log(questions.answers);
+
+function checkAnswer(event){
+    event.target;
+    var ans = event.target.textContent
+    console.log(event);
+    console.log(event.target)
+    console.log(event.target.textContent)
+    if (questions.answers.includes(ans)) {
+      textContent="Right!";
+    }
+      else {
+      textContent="Wrong!";
+      secondsLeft=secondsLeft-10;
+      }
+}   
+
+
+  
+
 function nextQuestion(){
   questionIndex++;
-  if questionIndex<5 {
-  displayQuestion(questionIndex);
-  } else
+  if (questionIndex<5) {}
   allDone ();
   //stop timer in allDone
 }
-var timeEl = document.querySelector(".time");
-
-// Selects element by id
-var mainEl = document.getElementById("main");
 
 
 function setTime() {
   // Sets interval in variable
   var timerInterval = setInterval(function() {
+    secondsLeft=60;
     secondsLeft--;
-    timeEl.textContent = "Time:" + secondsLeft;
+    timer.textContent = "Timer:" + secondsLeft;
 
     if(secondsLeft === 0) {
       // Stops execution of action at set interval
       clearInterval(timerInterval);
       // Calls function to create and append image
-      sendMessage();
+      allDone();
     }
-
   }, 1000);
 }
-
-question1 = "The acronym JSON stands for";
-choice1 = ["JavaScript Open Nomenclature","JavaSript Object Nomenclature","JavaScript Open Network","JavaScript Object Notation"];
-answer1 = "JavaScript Object Notation";
-question2 = "JSON is commonly used for";
-choice2 = ["sending data between computers.","storing JS objects as JSON strings in local storage.","transmitting data in web applications.","All of the above"];
-answer2 = "All of the above";
-question3 = "The acronym API stands for";
-choice3 = ["Advanced Programming Initiative","Application Programming Interface","Applied Performance Interoperability","Adverse Predictable Interference"];
-answer3 = "Application Programming Interface";
-question4 = "The acronym DOM stands for";
-choice4 = ["Document Object Model","Domain Operability Management","Definitive Operating Model","Domain Operating Model"];
-answer4 = "Document Object Model";
-question5 = "Which statement is not true?"
-choice5 = ["A method is a prebuilt function.","An object is a collection of properties.","A declared function independently executes a particular task.","Georgia Tech is a premiere, technical, educational institution!"];
-*/
